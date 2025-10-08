1c9b3d21c506122c0521e4a79bba90b6fa34b265
KlondikeDev <KlondikeDev@proton.me>
2025-10-07 19:15:54 -0700

    Rewrote echo.c, added kdeclare. - First utility rewrite.

--- echo.c
+++ echo.c
@@ -1,13 +1,17 @@
 #include "types.h"
 #include "stat.h"
 #include "user.h"
+#include "kdeclare.h"
 
-int
-main(int argc, char *argv[])
+int main(int argc, char *argv[])
 {
-  int i;
 
-  for(i = 1; i < argc; i++)
-    printf(1, "%s%s", argv[i], i+1 < argc ? " " : "\n");
+  for(int i = 1; i < argc; i++){
+    printf(stdio, "%s", argv[i]);
+    if(i + 1 < argc){
+      printf(stdio, " ");
+    }
+  }
+  printf(stdio, "\n");
   exit();
 }
