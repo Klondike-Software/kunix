a54063f82093e8030caec64c54ba28fe3df9d064
KlondikeDev <KlondikeDev@proton.me>
2025-10-07 19:28:10 -0700

    Changed README and added comments. Slight change to cat.

--- cat.c
+++ cat.c
@@ -1,39 +1,40 @@
+// Rewrite NEEDED.
+
 #include "types.h"
 #include "stat.h"
 #include "user.h"
+#include "kdeclare.h"
 
-char buf[512];
+char buffer[512];
 
-void
-cat(int fd)
+void cat(int fd)
 {
   int n;
 
-  while((n = read(fd, buf, sizeof(buf))) > 0) {
-    if (write(1, buf, n) != n) {
-      printf(1, "cat: write error\n");
+  while((n = read(fd, buffer, sizeof(buffer))) > 0) {
+    if (write(stdio, buffer, n) != n) {
+      printf(stdio, "cat: write error\n");
       exit();
     }
   }
   if(n < 0){
-    printf(1, "cat: read error\n");
+    printf(stdio, "cat: read error\n");
     exit();
   }
 }
 
-int
-main(int argc, char *argv[])
+int main(int argc, char *argv[])
 {
-  int fd, i;
+  int fd;
 
   if(argc <= 1){
     cat(0);
     exit();
   }
 
-  for(i = 1; i < argc; i++){
+  for(int i = 1; i < argc; i++){
     if((fd = open(argv[i], 0)) < 0){
-      printf(1, "cat: cannot open %s\n", argv[i]);
+      printf(stdio, "cat: cannot open %s\n", argv[i]);
       exit();
     }
     cat(fd);
--- echo.c
+++ echo.c
@@ -1,3 +1,5 @@
+// Everyone knows what an echo is!
+
 #include "types.h"
 #include "stat.h"
 #include "user.h"
@@ -5,13 +7,15 @@
 
 int main(int argc, char *argv[])
 {
-
+  // Loop through all of the arguments, skipping argv[0] because that is the program name.
   for(int i = 1; i < argc; i++){
     printf(stdio, "%s", argv[i]);
+    // print a space between arguments
     if(i + 1 < argc){
       printf(stdio, " ");
     }
   }
+  // newline at end.
   printf(stdio, "\n");
   exit();
 }
